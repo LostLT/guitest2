ef580494ecbc35ef73a93948868da1abd94af495
LostLT <maladuir@yahoo.com>
2014-08-14 16:03:59 -0400

    kiutg
    kgh

---
+++ GUItest/Assets/ScriptDefines.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptDefines : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		#if UNITY_TIZEN
+			Debug.Log ("Tizen Script define fired");
+		#endif
+
+		#if UNITY_BLACKBERRY
+			Debug.Log ("Blackberry Script define fired");
+		#endif
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
